5d17b9ed51a16453b4a3d1afe302e00019e8af85
Douglas-Pressley <37948112+Douglas-Pressley@users.noreply.github.com>
2018-03-30 23:49:12 -0500

    Add files via upload

---
+++ TaxCalculation/TaxCalculation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaxCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 79.95;
+            double tax1 = 0.06;
+            int tax2 = 6;
+            Console.WriteLine("The price of the item including tax is " + Tax(price, tax1));
+            Console.WriteLine("Please pay " + Tax(price, tax2));
+        }
+        public static string Tax(double x, double y)
+        {
+            return (x + (x * y)).ToString("C");
+        }
+        public static string Tax(double x, int y)
+        {
+            return (x + (x * y / 100)).ToString("C");
+        }
+    }
+}
